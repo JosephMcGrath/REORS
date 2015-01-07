bc6ab604de41d3e56378e8115ff64b1673799121
JosephMcGrath <joseph.mcgrath@outlook.com>
2015-01-07 11:39:58 +0000

    Added TempClean
    Function to easily remove all temporary files relating to the REORS or
    raster packages.

---
+++ R/TempClean.R
@@ -0,0 +1,15 @@
+TempClean <- function(tDir = tempdir()){
+#Cleans the temporary directory of all files belonging to the REORS package.
+#As you might imagine, this cannot be undone, take care with this function.
+#Searches for file beginning with "REORS" and ending in "tif", shouldn't
+# overlap with anything.
+#
+#Args:
+#  The folder to search and clear for temporary files.
+#
+#Returns:
+#  None, deletes files from system.
+
+  file.remove(paste(tDir, list.files(tDir, "REORS.*tif"), sep = "\\"))
+  removeTmpFiles(h = 0)
+}
