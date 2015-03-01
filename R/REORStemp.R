abc2958a8a845e0fa9c3e252a66488998eb25009
JosephMcGrath <joseph.mcgrath@outlook.com>
2015-03-01 13:33:16 +0000

    Deleted unintentionally included code.
    Previous loading issues were due to a in-progress file being included in
    the wrong folder (LandsatProcessing2.R). Have now cleaned that up,

--- R/REORStemp.R
+++
@@ -1,5 +0,0 @@
-REORStemp <- function(){
-#Wrapper function to generate temporary file names.
-#Separated to allow easier changing and to keep things consistent.
-  return(tempfile(pattern = "REORS - "))
-}
