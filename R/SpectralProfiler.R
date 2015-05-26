6ed999f5a241123b1c453fcc57c033ea4b42e102
JosephMcGrath <joseph.mcgrath@outlook.com>
2015-05-26 10:36:47 +0100

    Updated SpectralProfiler
    Re-worked SpectralProfiler, largely from the ground up. Now works
    significantly better with Spatial* objects. Functionality for writing
    csv's inside the function has been entirely removed as writing out data
    is easily done, and this way can be carried out by the user in whatever
    format they prefer,
    
    Plotting inside the function is planned to be added - currently the
    function is basically a wrapper for extract() with limited added
    functionality.

--- R/SpectralProfiler.R
+++ R/SpectralProfiler.R
@@ -1,158 +1,148 @@
-SpectralProfiler <- function(rasterIn, type = "points", nProfile = 1,
- plotL = NA, stand = FALSE, silent = TRUE, shapeIn = NULL, csvOut = NULL){
-#Calculates the spectral profile of a set zone.
-#For polygoins, calculates the mean values.
+SpectralProfiler <- function(rasterIn, shapeIn = "point", nProfile = 1,
+ returnSpatial = FALSE, plotOut = TRUE, silent = TRUE){
+#Calculates the spectral properties of a set of shapes.
+#For points, the profile for each point is given.
+#For lines, the profile along the line is given.
+#For polygons, the average value for pixels under the polygon.
 #
-#To-do notes:
-#-All options should output a spatial* object, currently points only gives
-# coordinates.
-#-Proper key for profile plotting.
-#
-#Requires: RasterLoad
+#Requires:
+#  RasterLoad
 #
 #To do:
-#   Merge "type" and "shapeIn" arguments to automatically detect shapefiles
+#  -Plot results out
+#  -Print numbers onto shapes as they're drawn
 #
 #Args:
 #  rasterIn: Raster* object to extract values from.
-#  type: What type of input should be used?
-#   -points: Profiles for each point.
-#   -line: Profiles of changing spectral values along a line.
-#   -poly : Profiles of mean values over polygons.
-#   -shape: Profiles using a pre-defined shapefile, not yet implemented.
-#  nProfile: Number of profiles to take (ignored for "line" & "shape")
-#  plotL: Layer to plot, NA for not plotting.
-#  stand: Should the data be standardised before plotting?
-#  silent: Should progress be printed to the console?
-#  shapeIn: If type is "shape", this is where the shapefile is given.
-#  csvOut: A string of the file name to write the profile to. If omitted, the
-#   file is not written.
+#  shapeIn: Either a Spatial* object, or one of the following strings:
+#   -"point": To draw points on the plot window.
+#   -"line": To draw lines on the plot window.
+#   -"poly": To draw polygons on the plot window.
+#  nProfile: If creating points or polygons, how many should be created.
+#  returnSpatial: If the spectral profiles should be returned as a
+#   SpatialDataFrame rather than a matrix.
+#  silent: Should information about progress be returned?
 #
 #Returns:
-#  A list of two items:
-#   -Matrix of DN values
-#   -Coordinates/shapefile that was used to create them.
+#  Either a matrix of digital numbers for each point/line/polygon provided or
+#   a Spatial* object with the digital numbers attached.
+#  For points, values are collected per-pixel.
+#  For lines, each pixel along each line is returned in order (as a list of
+#   matrices). Due to the many-one relationship of values to lines, spatial
+#   results are returned as a list of shapes and values.
+#  For polygons, the average value for each polygon is returned.
 
   library("raster")
   library("REORS")
   
   rasterIn <- RasterLoad(rasterIn, retForm = "stack")
   
-  specProf <- matrix(nrow = nProfile, ncol = nlayers(rasterIn))
-  
-  #Common typos
-  if(type == "point") type <- "points"
-  if(type == "polygon") type <- "poly"
-  if(type == "polygons") type <- "poly"
-  if(type == "polys") type <- "poly"
-  if(type == "lines") type <- "line"
-  
-#--Calculation using shape-files----------------------------------------------
-  if(type == "shape"){
-  
-    if(class(shapeIn)[1] == "SpatialLines" |
-     class(shapeIn)[1] == "SpatialLinesDataFrame"){
-      specProf <- t(extract(rasterIn, shapeIn, along = TRUE)[[1]])
-      type <- "line" #For plotting later
-      #Also limit to one line
-    } else if(class(shapeIn) == "list"){
-      for(i in 1:length(shapeIn)){
-        specProf <- extract(rasterIn, shapeIn[[i]], "mean")
+#--If creating Spatial* object from scratch, handle it here-------------------
+  if(is.character(shapeIn)){                                                  #Print numbers onto plotted shapes.
+    if(length(shapeIn == 1)){
+      if(shapeIn == "point"){
+        #Doing this in a more-complex manner so that this section behaves the
+         #same as lines and polygons (plotting along the way).
+        shapeIn <- locator(n = 1)
+        shapeIn <- cbind(shapeIn$x, shapeIn$y)
+        shapeIn <- SpatialPoints(shapeIn)
+        plot(shapeIn[1, ], col = rainbow(nProfile)[1], add = TRUE)
+        if(nProfile > 1) for(i in 2:nProfile){
+          temp <- locator(n = 1)
+          temp <- cbind(temp$x, temp$y)
+          temp <- SpatialPoints(temp)
+          shapeIn <- rbind(shapeIn, temp)
+          plot(shapeIn[i, ], col = rainbow(nProfile)[i], add = TRUE)
+        }
+      } else if(shapeIn == "line"){
+        shapeIn <- drawLine(col = rainbow(nProfile)[1])
+        if(nProfile > 1) for(i in 2:nProfile){
+          temp <- drawLine(col = rainbow(nProfile)[i])
+          temp <- spChFIDs(temp, as.character(length(shapeIn) + 1))
+          shapeIn <- rbind(shapeIn, temp)
+        }
+      } else if(shapeIn == "poly"){
+        shapeIn <- drawPoly(col = rainbow(nProfile)[1])
+        if(nProfile > 1) for(i in 2:nProfile){
+          temp <- drawPoly(col = rainbow(nProfile)[i])
+          temp <- spChFIDs(temp, as.character(length(shapeIn) + 1))
+          shapeIn <- rbind(shapeIn, temp)
+        }
+      } else {
+        stop("Invalid character string for shapeIn.")
       }
-    } else if(class(shapeIn) == "matrix"){
-      specProf <- rasterIn[cellFromXY(rasterIn, shapeIn)]
+      
+      crs(shapeIn) <- crs(rasterIn)
     } else {
-      specProf <- extract(rasterIn, shapeIn, "mean")
-    }
-    
-    AOI <- shapeIn
-  }
-  
-#--Calculation using polygons-------------------------------------------------
-#spRbind(temp1, spChFIDs(temp2, "2"))
-#crs
-  if(type == "poly"){
-    AOI <- list()
-    if(!is.na(plotL)) plot(rasterIn, plotL)
-    
-    if(!silent) cat("Click on the plot window to mark on areas to test\n")
-    
-    for(i in 1:nProfile){
-      AOI <- append(AOI, drawPoly(col = rainbow(nProfile)[i]))
-    }
-    
-    for(i in 1:length(AOI)){
-      specProf[i, ] <- extract(rasterIn, AOI[[i]], fun = mean)
+      stop("shapeIn may only take one argument.")
     }
   }
   
-#--Calculation using points---------------------------------------------------
-#spRbind(temp1, spChFIDs(temp2, "2"))
-#crs
-  if(type == "points"){
-    if(!is.na(plotL)) plot(rasterIn, plotL)
-    AOI <- locator(n = nProfile)
-    AOI <- cbind(AOI$x, AOI$y)
-    specProf <- rasterIn[cellFromXY(rasterIn, AOI)]
-  }
-  
-#--Calculation using lines----------------------------------------------------
-#spRbind(temp1, spChFIDs(temp2, "2"))
-#crs
-  if(type == "line"){
-    if(!is.na(plotL)) plot(rasterIn, plotL)
-    
-    if(!silent) cat("Click on the plot window to mark on areas to test\n")
-    
-    AOI <- drawLine()
-    
-    specProf <- t(extract(rasterIn, AOI, along = TRUE)[[1]])
-  }
-  
-#--Plot the profiles and return matrix----------------------------------------
-  if(stand){
-    specProf <- apply(
-     X = specProf,
-     MARGIN = 2,
-     FUN = function(x) (x - min(x)) / (max(x) - min(x))
-    )
-  }
-  
-  if(!silent) cat("Plotting stats\n")
-  
-  if(type == "line"){
-    xLabel <- "Distance (pixels)"
-    colnames(specProf) <-1:ncol(specProf)
+#--Extract the data from the raster-------------------------------------------
+#Desired format is a matrix with cols for bands and rows for observations
+  extractType <- class(shapeIn)[[1]]
+  if(length(grep("SpatialPolygons", extractType)) > 0){
+    dataOut <- extract(rasterIn, shapeIn, fun = mean)
+  } else if(length(grep("SpatialLines", extractType)) > 0){
+    temp <- extract(rasterIn, shapeIn, along = TRUE)
+    for(i in 1:length(temp)){
+      rownames(temp[[i]]) <- sprintf("L%sP%s", i, 1:nrow(temp[[i]]))
+    }
+    dataOut <- temp[[1]]
+    if(length(temp) > 1) for(i in 2:length(temp)){
+      dataOut <- rbind(dataOut, temp[[i]])
+    }
+  } else if(length(grep("SpatialPoints", extractType)) > 0){
+    dataOut <- extract(rasterIn, shapeIn)
   } else {
-    xLabel <- "Band #"
-    colnames(specProf) <- sprintf("Band %s", 1:ncol(specProf))
-  }
-  if(stand){
-    yLabel <- "Standardised DN"
-  } else {
-    yLabel <- "DN"
+    stop("Unable to recognise Spatial* object.")
   }
   
-  plot(c(1, ncol(specProf)),
-   c(min(specProf, na.rm = TRUE), max(specProf, na.rm = TRUE)),
-   type = "n", xlab = xLabel, ylab = yLabel
-  )
+#--Plot the data if requested-------------------------------------------------
+#Currently have issues with non-standard plot windows, e.g. plotRGB
+#As data can be easily plotted outside of the function, this isn't really a
+ #major issue.
+ 
+#Have an option to standardise all plots to between the same ranges.
   
-  for(i in 1:nrow(specProf)){
-    if(type != "line"){
-      points(1:ncol(specProf), specProf[i, ], 
-       col = rainbow(nrow(specProf))[i], pch = 4
-      )
+#  if(plotOut){
+#    if(length(grep("SpatialPolygons", extractType)) > 0){
+#      plot(x = c(1,ncol(dataOut)), y = c(0, max(dataOut)), type = "n", xlab = "Band #", ylab = "Digital number")
+#      for(i in 1:nrow(dataOut)){
+#        lines(dataOut[i, ], col = rainbow(nrow(dataOut))[i])
+#      }
+#    } else if(length(grep("SpatialLines", extractType)) > 0){
+#      plot(x = c(1,ncol(dataIn)), y = c(0, max(dataOut)), type = "n", xlab = "Distance (pixels)", ylab = "Digital number")
+#                                                                              #Unsure how to plot for multiple line object
+#    } else if(length(grep("SpatialPoints", extractType)) > 0){
+#      plot(x = c(1,ncol(dataOut)), y = c(0, max(dataOut)), type = "n", xlab = "Band #", ylab = "Digital number")
+#      for(i in 1:nrow(dataOut)){
+#        lines(dataOut[i, ], col = rainbow(nrow(dataOut))[i])
+#      }
+#    }
+#  }
+  
+#--Output extracted data------------------------------------------------------
+  if(returnSpatial){
+    if(length(grep("SpatialLines", extractType)) == 0){
+      rownames(dataOut) <- names(shapeIn)
+      dataOut <- data.frame(dataOut)
     }
-    lines(1:ncol(specProf), specProf[i, ], col = rainbow(nrow(specProf))[i])
+    if(length(grep("SpatialPolygons", extractType)) > 0){
+      dataOut <- SpatialPolygonsDataFrame(shapeIn, dataOut)
+    } else if(length(grep("SpatialLines", extractType)) > 0){
+      dataOut <- list(shapeIn, dataOut)
+      
+      #Return pixels along the line as points in this case
+      #temp <- extract(rasterIn, AOI, along = TRUE, cellnumbers = TRUE)
+      #for(i in 1:length(temp)){
+      #  temp[[i]] <- temp[[i]][, 1]
+      #  temp[[i]] <- cellFromXY(rasterIn, temp[[i]])
+      #}
+      
+    } else if(length(grep("SpatialPoints", extractType)) > 0){
+      dataOut <- SpatialPointsDataFrame(shapeIn, dataOut)
+    }
   }
-  
-#--Write to file
-  if(!is.null(csvOut)){
-    write.table(specProf, file = sprintf("%s.csv", csvOut),
-     sep = ",", row.names = FALSE
-    )
-  }
-  
-  return(list("Profile" = specProf, "Area used"= AOI))
+  return(dataOut)
 }
